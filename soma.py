1dff4317afb523c4a4ea763e1ee44d8b3c958ca0
Isabela Fernandes <138222925+isafernanndes@users.noreply.github.com>
2024-06-25 08:33:53 -0300

    Create soma.py

---
+++ soma.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número: '))
+
+n2 = int(input('Digite mais um número: '))
+
+soma = n1 + n2
+
+print('O primeiro valor digitado foi {} e o outro valor digitado foi {}, a soma dos dois números é {}'.format(n1, n2, soma))
